21dd76c98ab361c68824b1a0895191a613324d29
Elena-Shagdurova <shagdurovva.elena@gmail.com>
2023-06-01 10:34:39 -0400

    commit6

--- src/com/automation/BinaryTreeInorderTraversal94.java
+++ src/com/automation/BinaryTreeInorderTraversal94.java
@@ -11,7 +11,7 @@ import java.util.List;
      TreeNode(int val){
          this.val=val;
      }
-     TreeNode(int val, TreeNode left, TreeNode right){
+     TreeNode(int val, TreeNode right, TreeNode left){
          this.val = val;
          this.left = left;
          this.right = right;
