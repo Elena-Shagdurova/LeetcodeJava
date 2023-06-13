086217cbdc158bd4772f9d0ea1f51c724e85362f
ChimitaBazarova <chimitab@gmail.com>
2023-06-13 15:11:43 -0400

    kkl

--- src/com/automation/HiTarbagatai.java
+++ src/com/automation/HiTarbagatai.java
@@ -3,5 +3,6 @@ package com.automation;
 public class HiTarbagatai {
     public static void main(String[] args) {
         System.out.println("Nu zayaw pogodi");
+        System.out.println("One more time ))) hi girls");
     }
 }
