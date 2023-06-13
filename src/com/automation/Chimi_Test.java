e5e22e19459fd78093d1bb3a935a6a9596ecb070
ChimitaBazarova <chimitab@gmail.com>
2023-06-13 15:01:16 -0400

    onemoretimeChimi

--- src/com/automation/Chimi_Test.java
+++ src/com/automation/Chimi_Test.java
@@ -1,2 +1,7 @@
-package com.automation;public class Chimi_Test {
+package com.automation;
+
+public class Chimi_Test {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
 }
